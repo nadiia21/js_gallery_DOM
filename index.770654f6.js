65496d982c83fe26faab9a35b2c847e3c22407ec
Nadiia <zoryvchaknv@gmail.com>
2024-12-03 14:37:02 +0200

    make build

---
+++ index.770654f6.js
@@ -0,0 +1,2 @@
+var e=Array.from(document.querySelectorAll("a")),r=document.querySelector("#largeImg");e.forEach(function(e){e.addEventListener("click",function(c){c.preventDefault(),r.src=e.href})});
+//# sourceMappingURL=index.770654f6.js.map
